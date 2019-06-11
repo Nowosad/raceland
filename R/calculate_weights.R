710dff256eaf5726ffab9e0efe062d7b356e16ac
Nowosad <tupiszakaczki@gmail.com>
2019-06-11 19:14:05 +0200

    starts with r weigths code

---
+++ R/calculate_weights.R
@@ -0,0 +1,11 @@
+y = cats
+
+calculate_weights = function(x, y, size){
+  x_areas = motifel_areas(x = as.matrix(x), size = size)
+
+  y_sums = motifel_sums()
+
+  motifel_sum_area = rowSums(x_areas)
+
+
+}
