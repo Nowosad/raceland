67b59762262620e4399ce257e200939858d7662e
Nowosad <tupiszakaczki@gmail.com>
2019-06-20 15:05:17 +0200

    adds calculate metrics

---
+++ R/calculate_metrics.R
@@ -0,0 +1,29 @@
+#' Title
+#'
+#' @param x
+#' @param w
+#' @param directions
+#' @param fun
+#' @param size
+#' @param na_action
+#' @param base
+#' @param ordered
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library(sd)
+#' library(raster)
+#' x = create_realization(perc_raster)
+#' w = create_weights(x, perc_raster, size = 100)
+#' df = calculate_metrics(x, w, neighbourhood = 4, fun = "mean")
+#' df = calculate_metrics(x, w, neighbourhood = 4, fun = "mean", size = 100)
+calculate_metrics = function(x, w, neighbourhood, fun, size = NULL, na_action = "replace", base = "log2", ordered = TRUE){
+  if (is.null(size)){
+    size = 0
+  }
+  get_metrics(x = raster::as.matrix(x), w = raster::as.matrix(w),
+              directions = as.matrix(neighbourhood), fun = fun,
+              na_action = na_action, base = base, ordered = ordered, size = size)
+}
