4d2900b9ad6c826dcc3b8fa64c11bdb47170a261
Nowosad <tupiszakaczki@gmail.com>
2019-07-17 19:18:19 +0200

    adds density tests

---
+++ tests/testthat/test-densities.R
@@ -0,0 +1,10 @@
+real_raster = create_realizations(prop_raster, n = 1)
+weig_raster = create_densities(real_raster, prop_raster, size = 10)
+
+test_that("total population stays the same for a realization", {
+  expect_equal(sum(prop_raster[], na.rm = TRUE),
+               sum(weig_raster[[1]][], na.rm = TRUE))
+})
+
+
+
