d764a841f40e8a31a913b498b4b64f53cd4fb3c8
Nowosad <tupiszakaczki@gmail.com>
2019-08-05 18:33:12 +0200

    improves docs

--- R/zones_to_raster.R
+++ R/zones_to_raster.R
@@ -3,7 +3,7 @@
 #' @param v An sf object (POLYGON or MULTIPOLYGON)
 #' @param resolution A numeric vector of length 1 or 2 to set the resolution
 #' @param variables A character vector with columns names from `v`.
-#' The values from these columns will be rasterized.
+#' The values from these columns will be (1) recalculated to densities and then (2) rasterized.
 #' Each column will be represented as an layer in the output RasterStack.
 #' @param ... Additional arguments as for [fasterize::fasterize()]
 #'
@@ -14,7 +14,8 @@
 #' library(sf)
 #' library(raster)
 #' plot(pop_vector)
-#' popdens_raster = zones_to_raster(pop_vector, resolution = 100, variables = c("NHW", "NHB", "NHAS", "NHOTHER", "HISP"))
+#' popdens_raster = zones_to_raster(pop_vector, resolution = 100,
+#'                                  variables = c("NHW", "NHB", "NHAS", "NHOTHER", "HISP"))
 #' plot(popdens_raster)
 zones_to_raster = function(v, resolution, variables, ...){
   out = if (requireNamespace("pbapply", quietly = TRUE)){
