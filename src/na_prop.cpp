74151730a3e141812829b488d869ce72a0fae562
nowosad <dukekubas@gmail.com>
2019-08-23 09:23:15 +0200

    specify use of c++11

--- src/na_prop.cpp
+++ src/na_prop.cpp
@@ -11,19 +11,6 @@ double na_prop(IntegerMatrix x) {
   return na_of_na / na_of_cells;
 }
 
-
-// calculates a proportion of cells with NA's
-// [[Rcpp::export]]
-double na_prop2(const IntegerMatrix& x) {
-
-  double na_of_cells = x.length();
-  double na_of_na = std::count_if(x.begin(), x.end(),
-                                  IntegerVector::is_na);
-
-  return na_of_na / na_of_cells;
-}
-
-
 /*** R
 a = matrix(c(1:9), ncol = 3)
 b = matrix(c(1:5, rep(NA, 4)), ncol = 3)
--- src/na_prop.h
+++ src/na_prop.h
@@ -1,6 +1,8 @@
 #ifndef na_prop_H
 #define na_prop_H
 #include "Rcpp.h"
+#include <algorithm>     // for count_if
+// [[Rcpp::plugins(cpp11)]]
 
 // [[Rcpp::export]]
 double na_prop(Rcpp::IntegerMatrix x);
