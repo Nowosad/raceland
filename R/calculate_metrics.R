4d0190cd336a85fa4f7df3cde348a1f3e47b0360
Nowosad <tupiszakaczki@gmail.com>
2019-09-04 11:52:16 +0200

    fixes calculate_metrics bug

--- R/calculate_metrics.R
+++ R/calculate_metrics.R
@@ -48,8 +48,11 @@ calculate_metrics = function(x, w, neighbourhood, fun, size = NULL, shift = NULL
     shift = size
   }
   out = if (requireNamespace("pbapply", quietly = TRUE)){
-    pbapply::pbmapply(calculate_metric, raster::as.list(x), raster::as.list(w), neighbourhood = neighbourhood,
-                      fun = fun, size = size, na_action = na_action, base = base, ordered = ordered, threshold = threshold,
+    pbapply::pbmapply(calculate_metric, raster::as.list(x), raster::as.list(w),
+                      neighbourhood = neighbourhood, fun = fun,
+                      size = size, shift = shift,
+                      na_action = na_action, base = base,
+                      ordered = ordered, threshold = threshold,
                       SIMPLIFY = FALSE)
   } else {
     out = mapply(calculate_metric, raster::as.list(x), raster::as.list(w),
